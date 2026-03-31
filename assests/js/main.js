612a261aadecdd5bcebf231f440e4e37297723a4
cengiz arslan <cengizarslan4006@gmail.com>
2026-03-31 17:12:47 +0300

    Add shopping cart functionality and form submission

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,1 +1,14 @@
+let sepetSayisi = 0;
+
+function sepeteEkle(urunAdi) {
+  sepetSayisi++;
+  document.getElementById("sepet").innerText = sepetSayisi;
+  alert(`${urunAdi} sepete eklendi!`);
+}
+
+document.getElementById("contactForm")?.addEventListener("submit", (e) => {
+  e.preventDefault();
+  document.getElementById("formSonuc").innerText = "Mesajınız başarıyla gönderildi!";
+  e.target.reset();
+});
 
